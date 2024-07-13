a76317a5307221dfe8a8f942a325e73bfe857940
Apinan Woratrakun <iamapinan@gmail.com>
2024-07-14 00:02:53 +0700

    update configs

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "your-project-id.firebaseapp.com",
-    databaseURL: "https://your-project-id.firebaseio.com",
-    projectId: "your-project-id",
-    storageBucket: "your-project-id.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyCKOR-oUZGZdO2-4lKkmzY0TO6UxqWG99g",
+    authDomain: "iobot-wnlrcf.firebaseapp.com",
+    databaseURL: "https://iobot-wnlrcf.firebaseio.com",
+    projectId: "iobot-wnlrcf",
+    storageBucket: "iobot-wnlrcf.appspot.com",
+    messagingSenderId: "857930725841",
+    appId: "1:857930725841:web:8eb810d596d83761ee08f0"
   };
